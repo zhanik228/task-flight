33ae07b17ace12a4bbf28db994ddd2d2ed462e48
zhanik228 <zhan142004@gmail.com>
2024-02-22 23:20:01 +0600

    added booking page

--- search.js
+++ search.js
@@ -9,29 +9,29 @@ const flightCardsWrapper = document.querySelector(".flight-cards__wrapper");
 
 flightCards.forEach((flight) => {
   console.log(flight.textContent);
-  //   flight.addEventListener("click", () => {
-  //     const isReturn = confirm("is it selected for there?");
+  flight.addEventListener("click", () => {
+    const isReturn = confirm("is it selected for there?");
 
-  //     if (!isReturn) {
-  //       if (selectedToReturnDisplay.children.length) {
-  //         if (
-  //           selectedToReturnDisplay.children[0].classList.contains("flight-card")
-  //         ) {
-  //           flightCardsWrapper.prepend(selectedToReturnDisplay.children[0]);
-  //         }
-  //       }
-  //       selectedToReturnDisplay.appendChild(flight);
-  //     } else {
-  //       if (selectedToThereDisplay.children.length) {
-  //         if (
-  //           selectedToThereDisplay.children[0].classList.contains("flight-card")
-  //         ) {
-  //           flightCardsWrapper.prepend(selectedToThereDisplay.children[0]);
-  //         }
-  //       }
-  //       selectedToThereDisplay.appendChild(flight);
-  //     }
-  //   });
+    if (!isReturn) {
+      if (selectedToReturnDisplay.children.length) {
+        if (
+          selectedToReturnDisplay.children[0].classList.contains("flight-card")
+        ) {
+          flightCardsWrapper.prepend(selectedToReturnDisplay.children[0]);
+        }
+      }
+      selectedToReturnDisplay.appendChild(flight);
+    } else {
+      if (selectedToThereDisplay.children.length) {
+        if (
+          selectedToThereDisplay.children[0].classList.contains("flight-card")
+        ) {
+          flightCardsWrapper.prepend(selectedToThereDisplay.children[0]);
+        }
+      }
+      selectedToThereDisplay.appendChild(flight);
+    }
+  });
 
   flight.setAttribute("draggable", true);
 
@@ -44,26 +44,28 @@ flightCards.forEach((flight) => {
   });
 });
 
-selectedToThereDisplay.addEventListener("drop", (event) => {
+selectedToThereDisplay.addEventListener("drop", handleThereDrop);
+selectedToThereDisplay.addEventListener("dragover", handleDragOver);
+
+selectedToReturnDisplay.addEventListener("drop", handleReturnDrop);
+selectedToReturnDisplay.addEventListener("dragover", handleDragOver);
+
+function handleThereDrop(event) {
   event.preventDefault();
   event.target.style.height = "auto";
   selectedToThereDisplay.appendChild(
     document.getElementById(event.dataTransfer.getData("text/plain"))
   );
-});
+}
 
-selectedToThereDisplay.addEventListener("dragover", (event) => {
-  event.preventDefault();
-});
-
-selectedToThereDisplay.addEventListener("touchend", (event) => {
+function handleReturnDrop(event) {
   event.preventDefault();
   event.target.style.height = "auto";
-  selectedToThereDisplay.appendChild(
+  selectedToReturnDisplay.appendChild(
     document.getElementById(event.dataTransfer.getData("text/plain"))
   );
-});
+}
 
-selectedToThereDisplay.addEventListener("touchmove", (event) => {
+function handleDragOver(event) {
   event.preventDefault();
-});
+}
